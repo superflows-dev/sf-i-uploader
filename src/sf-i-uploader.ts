4e84297b780556a67d85392f8fcbc9ea631f367b
Hrushi M <108924653+superflows-dev@users.noreply.github.com>
2024-04-15 23:52:48 +0530

    renamed part

--- src/sf-i-uploader.ts
+++ src/sf-i-uploader.ts
@@ -742,8 +742,8 @@ export class SfIUploader extends LitElement {
             htmlStr += '</div>';
           }
           htmlStr += '<div class="mt-20 w-100">';
-            htmlStr += '<div part="matches-title" >Extracted Text</div>';
-            htmlStr += '<div part="extracted-text" class="d-flex align-center mt-10">';
+            htmlStr += '<div part="matches-title">Extracted Text</div>';
+            htmlStr += '<div part="extracted-text" class="d-flex align-center">';
             htmlStr += '<sf-i-elastic-text text="'+this.inputArr[i]["arrWords"].join(' ')+'" minLength="100"></sf-i-elastic-text>';
             htmlStr += '</div>';
           htmlStr += '</div>';
